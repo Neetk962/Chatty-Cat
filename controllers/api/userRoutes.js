0441ce905b5c1dea0c68cca68a5815dcdbe4c014
TheMikal <131578548+TheMikal@users.noreply.github.com>
2023-08-20 17:45:43 -0400

    fixed login and logout issue

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
 const { User } =require('../../models');
 
-// GET /api/users
+// GET all users
+
+router.get("/", async (req, res) =>{
+    try {
+        const users = await User.findAll({});
+
+        res.status(200).json(users);
+    } catch (e) {
+        console.log(e);
+        res.status(500).send();
+    }
+});
+
+// create new user
 
 router.post('/', async (req, res) => {
     try {
@@ -55,6 +68,7 @@ router.post('/login', async (req, res) => {
         });
 
     } catch (err) {
+        console.log(err);
         res.status(400).json(err);
     }
 });
