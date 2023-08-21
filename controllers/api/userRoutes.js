613a3227ba4a88a81fcdb5273e4e8206bd401324
TheMikal <131578548+TheMikal@users.noreply.github.com>
2023-08-20 20:40:05 -0400

    fixed signup error

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -32,7 +32,11 @@ router.get("/:id", async (req, res) =>{
 
 router.post('/', async (req, res) => {
     try {
-        const userData = await User.create(req.body);
+        const userData = await User.create({
+            Username: req.body.Username,
+            email: req.body.email,
+            password: req.body.password,
+        });
 
         req.session.save(() => {
             req.session.user_id = userData.id;
@@ -43,6 +47,7 @@ router.post('/', async (req, res) => {
             res.status(200).json(userData);
         });
     } catch (err) {
+        console.log(err);
         res.status(400).json(err);
     }
 });
