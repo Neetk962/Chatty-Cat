6aceed5e7f50d6688cf6bafb2490d2c3dee1f73c
TheMikal <131578548+TheMikal@users.noreply.github.com>
2023-08-21 02:02:33 -0400

    fixed create post problems

--- public/javascript/createPost.js
+++ public/javascript/createPost.js
@@ -1,22 +1,22 @@
 const postFormHandler = async (event) => {
     event.preventDefault();
 
-    const blogTitle = document.querySelector('#post-title').value.trim;
-    const blogDescription = document.querySelector('#description').value.trim;
-    const blogDate = document.querySelector('#date_created').value.trim;
-    const blogBody = document.querySelector('#post-body').value.trim;
+    const name = document.querySelector('#post-title').value.trim();
+    const description = document.querySelector('#description').value.trim();
+    const date_created = document.querySelector('#date_created').value.trim();
+    const blogBody = document.querySelector('#post-body').value;
 
-    if (blogTitle && blogDescription && blogDate && blogBody) {
+    if (name && description && date_created) {
         const response = await fetch('/api/blogs', {
             method: 'POST',
-            body: JSON.stringify({ blogTitle, blogDescription, blogDate, blogBody }),
+            body: JSON.stringify({ name, description, date_created }),
             headers: { 'Content-Type': 'application/json' },
-        });
+        })
+        console.log(name, description, date_created);
 
         if (response.ok) {
             document.location.replace('/');
         } else {
-            console.log(error);
             alert('Failed to upload post.');
         }
     }
